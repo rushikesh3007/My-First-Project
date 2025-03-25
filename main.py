a5226b8f0ec12e894b58a1e89fa87165848e15ff
rushikesh3007 <ghogarerushi52@gmail.com>
2025-03-25 13:04:44 +0530

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-a=float(input("Enter First No:"))
-b=float(input("Enter Second No:"))
-c=a+b
-print("addition Is: ",c)
-
-c=a-b
-print("Subtraction Is: ",c)
-c=a*b
-print("Multiplication Is: ",c)
-
-c=a/b
-print("Division Is: ",c)
+print("Hello World")
 
