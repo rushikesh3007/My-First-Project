cbdbcc5756bdaf97e55cfdb3cf5bd0d827dccc46
rushikesh3007 <ghogarerushi52@gmail.com>
2025-03-25 12:59:41 +0530

    main.py

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ a=int(input("Enter First No:"))
 b=int(input("Enter Second No:"))
 c=a+b
 print("addition Is: ",c)
-print("Hello World")
+
+c=a-b
+print("Subtraction Is: ",c)
+c=a*b
+print("Multiplication Is: ",c)
+
+c=a/b
+print("Division Is: ",c)
 
